3e20aff52e1ea4063aba476497c3e383d1adaaca
tyang <tyang@i9i8.com>
2014-05-07 12:16:29 +0800

    lopp

---
+++ loop.py
@@ -0,0 +1,19 @@
+# x = 1
+# while x <= 100:
+# 	print x
+# 	x+=1
+
+# name = ''
+# while not name:
+# 	name = raw_input('Please enter your name: ')
+# print 'Hello, %s!' % name
+
+words = ['this','is','an','ex','parrot']
+for word in range(0,10):
+	print word
+
+d = {'x': 1, 'y': 2, 'z': 3}
+for key in d:
+	print key, 'corresponds to', d[key]
+#order undefined
+
